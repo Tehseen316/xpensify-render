607ffec7901845cc643113987fec4ea38549724d
Tehseen316 <tehseena316@gmail.com>
2025-07-10 22:21:36 +0100

    updated

--- Web Backend/index.js
+++ Web Backend/index.js
@@ -37,3 +37,8 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () =>
   console.log(`Server is running on this port... ${PORT} `)
 );
+
+
+app.get('/', (req, res) => {
+  res.send('Working ✅');
+});
